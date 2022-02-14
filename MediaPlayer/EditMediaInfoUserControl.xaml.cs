924d4ca52373a2f836f836e9534d67940a2714a4
CassandraThalia <W0449188@nscc.ca>
2022-02-14 19:47:51 -0400

    Add project files.

---
+++ MediaPlayer/EditMediaInfoUserControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for EditMediaInfoUserControl.xaml
+    /// </summary>
+    public partial class EditMediaInfoUserControl : UserControl
+    {
+
+        public delegate void SaveClickEvent(object sender, RoutedEventArgs e);
+        public event SaveClickEvent OnSaveClickEvent;
+
+        public EditMediaInfoUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.OnSaveClickEvent?.Invoke(sender, e);
+        }
+
+    }
+}
+
+//References:
+//https://stackoverflow.com/questions/61158159/handling-a-button-click-inside-a-user-control-from-the-main-window
---
+++ MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TagLib.File currentFile;
+        private bool mediaPlayerIsPlaying = false;
+        private bool userIsDraggingSlider = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if ((mediaPlayer.Source != null) && (mediaPlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+            {
+                progressBar.Minimum = 0;
+                progressBar.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                progressBar.Value = mediaPlayer.Position.TotalSeconds;
+            }
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if(fd.ShowDialog() == true)
+            {
+                if (System.IO.Path.GetExtension(fd.FileName) == ".mp3")
+                {
+                    currentFile = TagLib.File.Create(fd.FileName);
+                    buildSongInfo();
+                    fillUserForm();
+                    mediaPlayer.Source = new Uri(fd.FileName);
+                    if (currentFile.Tag.Pictures.Length >= 1)
+                    {
+                        TagLib.IPicture pic = currentFile.Tag.Pictures[0];
+                        MemoryStream ms = new MemoryStream(pic.Data.Data);
+                        ms.Seek(0, SeekOrigin.Begin);
+                        BitmapImage b = new BitmapImage();
+                        b.BeginInit();
+                        b.StreamSource = ms;
+                        b.EndInit();
+                        coverImg.Source = b;
+                    }
+                    else if (currentFile.Tag.Pictures.Length == 0)
+                    {
+                        coverImg.Source = new BitmapImage(new Uri(@"images\cover.jpg", UriKind.Relative));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Incompatible file type, please select an .mp3 file");
+                }
+            }
+            InitializeTimer();
+        }
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            songInfoBox.Visibility = Visibility.Collapsed;
+            coverImg.Visibility = Visibility.Collapsed;
+            editUserConotrol.Visibility = Visibility.Visible;
+        }
+        private void Player_Click(object sender, RoutedEventArgs e)
+        {
+            buildSongInfo();
+            songInfoBox.Visibility = Visibility.Visible;
+            coverImg.Visibility = Visibility.Visible;
+            editUserConotrol.Visibility = Visibility.Collapsed;
+        }
+
+        private void buildSongInfo()
+        {
+            string msg = "";
+            if (currentFile.Tag.Title != null)
+            {
+                msg += currentFile.Tag.Title + "\n";
+            }
+            else if (currentFile.Tag.Title == null)
+            {
+                msg += "Unknown Title \n";
+            }
+            if (currentFile.Tag.AlbumArtists.Length != 0)
+            {
+                msg += currentFile.Tag.AlbumArtists[0] + "\n";
+            }
+            else if (currentFile.Tag.AlbumArtists.Length == 0)
+            {
+                msg += "Unknown Artist \n";
+            }
+            if (currentFile.Tag.Album != null)
+            {
+                msg += currentFile.Tag.Album + "\n";
+            }
+            else if (currentFile.Tag.Album == null)
+            {
+                msg += "Unknown Album \n";
+            }
+            if (currentFile.Tag.Year != 0)
+            {
+                msg += currentFile.Tag.Year.ToString();
+            }
+            else if (currentFile.Tag.Year == 0)
+            {
+                msg += "Unknown Year";
+            }
+            songInfoBox.Text = msg;
+        }
+
+        private void fillUserForm()
+        {
+            if (currentFile != null)
+            {
+                editUserConotrol.titleBox.Text = currentFile.Tag.Title;
+                if (currentFile.Tag.AlbumArtists.Length > 0)
+                {
+                    editUserConotrol.artistbox.Text = currentFile.Tag.AlbumArtists[0];
+                }
+                else if (currentFile.Tag.AlbumArtists.Length == 0)
+                {
+                    editUserConotrol.artistbox.Text = "";
+                }
+                editUserConotrol.albumBox.Text = currentFile.Tag.Album;
+                if (currentFile.Tag.Year == 0)
+                {
+                    editUserConotrol.yearBox.Text = "";
+                }
+                else if (currentFile.Tag.Year != 0)
+                {
+                    editUserConotrol.yearBox.Text = currentFile.Tag.Year.ToString();
+                }
+            }
+        }
+
+        private void SaveMediaInfoClick(object sender, RoutedEventArgs e)
+        {
+            currentFile.Tag.Title = editUserConotrol.titleBox.Text;
+            currentFile.Tag.AlbumArtists = new string[] { editUserConotrol.artistbox.Text };
+            currentFile.Tag.Album = editUserConotrol.albumBox.Text;
+            currentFile.Tag.Year = Convert.ToUInt32(editUserConotrol.yearBox.Text);
+            currentFile.Save();
+            MessageBox.Show("Saved!");
+        }
+
+        private void PlayCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (mediaPlayer != null) && (mediaPlayer.Source != null);
+        }
+
+        private void PlayCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+            mediaPlayerIsPlaying = true;
+        }
+
+        private void PauseCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = mediaPlayerIsPlaying;
+        }
+
+        private void PauseCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void StopCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = mediaPlayerIsPlaying;
+        }
+
+        private void StopCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+            mediaPlayerIsPlaying = false;
+        }
+
+        private void sliProgress_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        private void sliProgress_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            mediaPlayer.Position = TimeSpan.FromSeconds(progressBar.Value);
+        }
+
+        private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            progressTimer.Text = TimeSpan.FromSeconds(progressBar.Value).ToString(@"hh\:mm\:ss");
+        }
+    }
+}
+
+
+//References:
+//https://dontpaniclabs.com/blog/post/2014/10/30/rounded-corners-with-transparent-backgrounds-in-wpf/
+//https://www.homeandlearn.co.uk/extras/speech/speech-open-file-button.html
+//https://stackoverflow.com/questions/61158159/handling-a-button-click-inside-a-user-control-from-the-main-window
+//https://www.wpf-tutorial.com/audio-video/how-to-creating-a-complete-audio-video-player/
+//https://stackoverflow.com/questions/17904184/using-taglib-to-display-the-cover-art-in-a-image-box-in-wpf
