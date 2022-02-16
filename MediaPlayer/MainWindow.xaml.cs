f7b60da40056166bdc997696b83044b80acc7d35
CassandraThalia <W0449188@nscc.ca>
2022-02-16 10:01:39 -0400

    Final touches

--- MediaPlayer/MainWindow.xaml.cs
+++ MediaPlayer/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace MediaPlayer
@@ -102,7 +92,7 @@ namespace MediaPlayer
             InitializeTimer();
         }
 
-        //If Edit button is clicked, stop media (or will cause save error) and show Edit Media Info User Control
+        //If Edit button is clicked, show Edit Media Info User Control
         private void Edit_Click(object sender, RoutedEventArgs e)
         {
             songInfoBox.Visibility = Visibility.Collapsed;
